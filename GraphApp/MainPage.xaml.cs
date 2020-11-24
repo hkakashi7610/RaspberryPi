7e09402381ff3177324297041d98306d8a0159c6
kakashi <33374712+hkakashi7610@users.noreply.github.com>
2020-11-24 23:44:31 +0900

    8つのIN信号対応

--- GraphApp/MainPage.xaml.cs
+++ GraphApp/MainPage.xaml.cs
@@ -1,20 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using LiveCharts;
+using LiveCharts.Uwp;
+using System;
+using Windows.Devices.Gpio;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-using LiveCharts;
-using LiveCharts.Uwp;
-using Windows.Devices.Gpio;
 
 // 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
 
@@ -25,9 +14,9 @@ namespace GraphApp
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        private const int LED_PIN = 27;
+        private readonly int[] arrPIN = new int[]{5,6,13,19,26,16,20,21};
         private DispatcherTimer timer;
-        private GpioPin pin;
+        private GpioPin[] arrGpioPin = new GpioPin[8];
         public SeriesCollection Sc { get; set; } = new SeriesCollection();
 
         public MainPage()
@@ -38,7 +27,7 @@ namespace GraphApp
             InitGraph();
 
             timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromMilliseconds(100);
+            timer.Interval = TimeSpan.FromMilliseconds(1000);
             timer.Tick += Timer_Tick;
             timer.Start();
 
@@ -52,21 +41,22 @@ namespace GraphApp
             // Show an error if there is no GPIO controller
             if (gpio == null)
             {
-                pin = null;
                 GpioStatus.Text = "There is no GPIO controller on this device.";
                 return;
             }
 
-            
-            pin = gpio.OpenPin(LED_PIN);
-            pin.Write(GpioPinValue.Low);
-            if (pin.IsDriveModeSupported(GpioPinDriveMode.InputPullUp))
+            for (int i = 0; i < arrPIN.Length; i++)
             {
-                pin.SetDriveMode(GpioPinDriveMode.InputPullUp);
-            }
-            else
-            {
-                pin.SetDriveMode(GpioPinDriveMode.Input);
+                arrGpioPin[i] = gpio.OpenPin(arrPIN[i]);
+                arrGpioPin[i].Write(GpioPinValue.Low);
+                if (arrGpioPin[i].IsDriveModeSupported(GpioPinDriveMode.InputPullUp))
+                {
+                    arrGpioPin[i].SetDriveMode(GpioPinDriveMode.InputPullUp);
+                }
+                else
+                {
+                    arrGpioPin[i].SetDriveMode(GpioPinDriveMode.Input);
+                }
             }
 
             GpioStatus.Text = "GPIO pin initialized correctly.";
@@ -80,47 +70,105 @@ namespace GraphApp
             /////////////////////////////////////
             Sc.Clear();
             Sc.Add(
-                new LineSeries                     //折れ線グラフ
+                new LineSeries
                 {
-                    //凡例名
-                    Title = "折れ線",
+                    //系列値
+                    Values = new ChartValues<int>(),
+                    //線の色
+                    Stroke = new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Colors.Red),
+                    //直線のスムージング（0：なし、省略：あり）
+                    LineSmoothness = 0,
+                    PointGeometry = DefaultGeometries.None,
+                });
+            Sc.Add(
+                new LineSeries
+                {
+                    //系列値
+                    Values = new ChartValues<int>(),
+                    //線の色
+                    Stroke = new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Colors.Yellow),
+                    //直線のスムージング（0：なし、省略：あり）
+                    LineSmoothness = 0,
+                    PointGeometry = DefaultGeometries.None,
+                });
+            Sc.Add(
+                new LineSeries
+                {
+                    //系列値
+                    Values = new ChartValues<int>(),
+                    //線の色
+                    Stroke = new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Colors.Purple),
+                    //直線のスムージング（0：なし、省略：あり）
+                    LineSmoothness = 0,
+                    PointGeometry = DefaultGeometries.None,
+                });
+            Sc.Add(
+                new LineSeries
+                {
+                    //系列値
+                    Values = new ChartValues<int>(),
+                    //線の色
+                    Stroke = new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Colors.Green),
+                    //直線のスムージング（0：なし、省略：あり）
+                    LineSmoothness = 0,
+                    PointGeometry = DefaultGeometries.None,
+                });
+            Sc.Add(
+                new LineSeries
+                {
+                    //系列値
+                    Values = new ChartValues<int>(),
+                    //線の色
+                    Stroke = new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Colors.Orange),
+                    //直線のスムージング（0：なし、省略：あり）
+                    LineSmoothness = 0,
+                    PointGeometry = DefaultGeometries.None,
+                });
+            Sc.Add(
+                new LineSeries
+                {
+                    //系列値
+                    Values = new ChartValues<int>(),
+                    //線の色
+                    Stroke = new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Colors.Blue),
+                    //直線のスムージング（0：なし、省略：あり）
+                    LineSmoothness = 0,
+                    PointGeometry = DefaultGeometries.None,
+                });
+            Sc.Add(
+                new LineSeries
+                {
+                    //系列値
+                    Values = new ChartValues<int>(),
+                    //線の色
+                    Stroke = new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Colors.White),
+                    //直線のスムージング（0：なし、省略：あり）
+                    LineSmoothness = 0,
+                    PointGeometry = DefaultGeometries.None,
+                });
+            Sc.Add(
+                new LineSeries
+                {
                     //系列値
                     Values = new ChartValues<int>(),
                     //線の色（省略：自動で配色されます）
-                    //Stroke = new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Colors.Red),
+                    Stroke = new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Colors.Chartreuse),
                     //直線のスムージング（0：なし、省略：あり）
-                    //LineSmoothness = 0,
-
+                    LineSmoothness = 0,
+                    PointGeometry = DefaultGeometries.None,
                 });
-
-            //Sc.Add(
-            //    new ColumnSeries                    //棒グラフ
-            //    {
-            //        Title = "棒",
-            //        Values = new ChartValues<long>(),
-            //        //Stroke = new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Colors.Red),
-            //    });
-
-            //Sc.Add(
-            //    new StepLineSeries                  //線グラフ
-            //    {
-            //        Title = "線",
-            //        Values = new ChartValues<long>(),
-            //        //Stroke = new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Colors.Red),
-            //    });
-
 
             /////////////////////////////////////
             //ステップ２:LiveChartの設定
             /////////////////////////////////////
             //凡例の表示位置
-            LC_Graph.LegendLocation = LegendLocation.Right;
+            //LC_Graph.LegendLocation = LegendLocation.Right;
 
             //軸の設定
             LC_Graph.AxisX.Clear();     //デフォルトで設定されている軸をクリア
-            LC_Graph.AxisX.Add(new Axis { Title = "横軸", FontSize = 20 });
+            //LC_Graph.AxisX.Add(new Axis { Title = "横軸", FontSize = 20 });
             LC_Graph.AxisY.Clear();
-            LC_Graph.AxisY.Add(new Axis { Title = "縦軸", FontSize = 20, MaxValue = 1, MinValue = 0 });
+            LC_Graph.AxisY.Add(new Axis { FontSize = 20, MinValue = 0 });
         }
 
         /// <summary>
@@ -141,16 +189,24 @@ namespace GraphApp
 
         }
 
+        private GpioPinValue pinValue;
+
         private void Timer_Tick(object sender, object e)
         {
-            Random rnd = new Random();
-
             //各系列に、それぞれ値を代入
             for (int iSeries = 0; iSeries < Sc.Count; iSeries++)
             {
-                GpioPinValue pinValue =  pin.Read();
+                GpioPinValue pinValue = arrGpioPin[iSeries].Read();
                 int value = (pinValue == GpioPinValue.High) ? 1 : 0;
-                Sc[iSeries].Values.Add(value);
+                
+                if (Sc[iSeries].Values.Count > 20)
+                {
+                    Sc[iSeries].Values.RemoveAt(0);
+                }
+                
+                Sc[iSeries].Values.Add(value + (iSeries * 3));   //上方向に積み上げるための3＋
+
+
             }
         }
     }
